48ef76c202b23dc1b0c1ad13436ffeec6614b51e
aswindevsp <aswindevsp@gmail.com>
2023-04-29 07:33:07 +0530

    finally

--- src/Firebase.js
+++ src/Firebase.js
@@ -1,6 +1,6 @@
-import firebase from 'firebase/app';
-import 'firebase/auth';
-import 'firebase/firestore';
+import firebase from 'firebase/compat/app';
+import 'firebase/compat/auth';
+import 'firebase/compat/firestore';
 
 const firebaseConfig = {
     apiKey: "AIzaSyC6GhkdrbMh6sKcTSv4WYheZqErrlV3o5E",
--- src/components/Display.js
+++ src/components/Display.js
@@ -1,64 +1,63 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react';
+import { firestore } from '../Firebase.js'
 
 const Display = () => {
-    const patient = [
-        { id: 1, name: "John Doe", age: 35, gender: "male", diagnosis: "flu" },
-        { id: 2, name: "Jane Smi", age: 42, gender: "female", diagnosis: "headache" },
-        { id: 3, name: "Bob John", age: 50, gender: "male", diagnosis: "diabetes" },
-        { id: 4, name: "Samantha ", age: 28, gender: "female", diagnosis: "broken leg" }
-      ];
-      
+  const [patient, setPatient] = useState([]);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      const patientsRef = firestore.collection('records');
+      const snapshot = await patientsRef.get();
+      const patients = snapshot.docs.map(doc => ({ id: doc.id, ...doc.data() }));
+      setPatient(patients);
+    };
+    fetchData();
+  }, []);
+
+
   return (
     <div>
-     <h1 className='text-2xl mx-auto font-bold'>patient Health history</h1>   
-    <div>
-    {patient.map((patients) => (
-        <div class="relative overflow-x-auto" key={patients.id}>
-        <table class="w-full text-sm text-left text-gray-500 ">
-            <thead class="text-xs text-gray-700 uppercase bg-gray-50  ">
-                <tr>
-                    <th scope="col" class="px-6 py-3">
-                    id
-                    </th>
-                    <th scope="col" class="px-6 py-3">
-                    Name
-                    </th>
-                    <th scope="col" class="px-6 py-3">
-                    Age
-                    </th>
-                    <th scope="col" class="px-6 py-3">
-                    diagnosis
-                    </th>
-                </tr>
-            </thead>
-            <tbody>
-                <tr class="bg-white border-b ">
-                    <th scope="row" class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap">
-                    {patients.id}
-                    </th>
-                    <td class="px-6 py-4">
-                    {patients.name}
-                    </td>
-                    <td class="px-6 py-4">
-                    {patients.age}
-                    </td>
-                    <td class="px-6 py-4">
-                    {patients.diagnosis}
-                    </td>
-                </tr>
-            </tbody>
+      <h1 className='text-2xl mx-auto font-bold'>patient Health history</h1>
+      <div>
+        <table className="w-full text-sm text-left text-gray-500">
+          <thead className="text-xs text-gray-700 uppercase bg-gray-50">
+            <tr>
+              <th scope="col" className="px-6 py-3">
+                id
+              </th>
+              <th scope="col" className="px-6 py-3">
+                Name
+              </th>
+              <th scope="col" className="px-6 py-3">
+                Age
+              </th>
+              <th scope="col" className="px-6 py-3">
+                diagnosis
+              </th>
+            </tr>
+          </thead>
+          <tbody>
+            {patient.map((patients) => (
+              <tr className="bg-white border-b" key={patients.id}>
+                <td className="px-6 py-4 font-medium text-gray-900 whitespace-nowrap">
+                  {patients.userId}
+                </td>
+                <td className="px-6 py-4">
+                  {patients.name}
+                </td>
+                <td className="px-6 py-4">
+                  {patients.age}
+                </td>
+                <td className="px-6 py-4">
+                  {patients.diagnosis}
+                </td>
+              </tr>
+            ))}
+          </tbody>
         </table>
-      {/* <div key={patients.id} className='flex items-center justify-center '>
-        <p className='mx-4 '></p>
-        <p className='mx-4 '></p>
-        <p className='mx-4 '>p>
-        <p className='mx-4 '>{patients.gender}</p>
-        <p className='mx-4 '></p>
-      </div> */}
       </div>
-    ))}
-  </div></div>
-  )
+    </div>
+  );
 }
 
 export default Display
