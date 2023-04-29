d1035dba2824162f7b4f8b79a04eeb5c15c3ddc3
rohit george <code.mastermind.07@gmail.com>
2023-04-29 07:06:17 +0530

    initial commit

---
+++ src/Firebase.js
@@ -0,0 +1,19 @@
+import firebase from 'firebase/app';
+import 'firebase/auth';
+import 'firebase/firestore';
+
+const firebaseConfig = {
+    apiKey: "AIzaSyC6GhkdrbMh6sKcTSv4WYheZqErrlV3o5E",
+    authDomain: "hack-b609c.firebaseapp.com",
+    databaseURL: "https://hack-b609c-default-rtdb.firebaseio.com",
+    projectId: "hack-b609c",
+    storageBucket: "hack-b609c.appspot.com",
+    messagingSenderId: "961590374257",
+    appId: "1:961590374257:web:699b2e8535d1e0e28490d0",
+    measurementId: "G-X4MLB1V59X"
+  };
+
+  firebase.initializeApp(firebaseConfig);
+  export const firestore = firebase.firestore();
+  export default firebase;
+
